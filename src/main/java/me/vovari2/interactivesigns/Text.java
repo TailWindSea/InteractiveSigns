93131f15de6d2dffb801ac257e3f1d3fb490b16b
Vovari2 <vovari4ek2@gmail.com>
2025-03-13 18:00:39 +0300

    Warning fix

--- src/main/java/me/vovari2/interactivesigns/Text.java
+++ src/main/java/me/vovari2/interactivesigns/Text.java
@@ -6,7 +6,6 @@ import me.vovari2.interactivesigns.utils.FileUtils;
 import me.vovari2.interactivesigns.utils.TextUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.HashMap;
