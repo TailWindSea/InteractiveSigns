e7bb262ff736ef1ed5c646f8873ae8e4a6d46b8c
Vovari2 <vovari4ek2@gmail.com>
2025-04-27 13:14:29 +0300

    Bug fix with 1.21.5

--- src/main/java/me/vovari2/interactivesigns/utils/VersionUtils.java
+++ src/main/java/me/vovari2/interactivesigns/utils/VersionUtils.java
@@ -6,7 +6,7 @@ import org.bukkit.Location;
 public class VersionUtils {
     public static Location getBlockCenter(Location blockLocation){
         String currentVersion = InteractiveSigns.getInstance().getServer().getMinecraftVersion();
-        return currentVersion.equals("1.21.4") || currentVersion.equals("1.21.3")
+        return currentVersion.matches("1\\.21\\.[345]")
                 ? blockLocation.clone().add(0.5, 0.5, 0.5) : blockLocation;
     }
 }
