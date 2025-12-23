517edce45ccb41438463732acc539b8191237a72
Vovari2 <vovari4ek2@gmail.com>
2025-12-23 18:41:43 +0300

    Bug fix

--- src/main/java/me/vovari2/interactivesigns/utils/VersionUtils.java
+++ src/main/java/me/vovari2/interactivesigns/utils/VersionUtils.java
@@ -6,7 +6,7 @@ import org.bukkit.Location;
 public class VersionUtils {
     public static Location getBlockCenter(Location blockLocation){
         String currentVersion = InteractiveSigns.getInstance().getServer().getMinecraftVersion();
-        return currentVersion.matches("^1\\.20(|\\.[23456]{1})$|^1\\.21(|\\.[12]{1})$")
+        return currentVersion.matches("|^1\\.21(|\\.1|2)$")
                 ? blockLocation : blockLocation.clone().add(0.5,0.5,0.5);
     }
 }
